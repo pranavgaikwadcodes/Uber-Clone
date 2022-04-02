41e5fdc02c1cfecdd7af1bfde318facba7a5916c
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2022-04-02 13:29:27 +0530

    working on HomeScreen

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store} >
-      <View style={styles.container}>
-        <Text>Lets Build Uber-Clone!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import React from 'react'
+import tw from 'twrnc'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View>
+          <Image 
+            // source={{
+            //     uri:
+                
+            // }}
+          />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
