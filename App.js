b3f3afd9e23af383201c0b05ec0ac25abc5ef720
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2022-04-05 22:19:44 +0530

    Done with module 1.

--- App.js
+++ App.js
@@ -2,20 +2,43 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 import { store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
 export default function App() {
 
+  const Stack = createStackNavigator();
 
   return (
     <Provider store={store} >
       <NavigationContainer>
         <SafeAreaProvider>
-          <HomeScreen />
+
+          <Stack.Navigator>
+
+            <Stack.Screen 
+              name='HomeScreen'
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+
+            <Stack.Screen 
+              name='MapScreen'
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            
+          </Stack.Navigator>
+
         </SafeAreaProvider>
       </NavigationContainer>
 
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import tw from 'twrnc'
 import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
     {
@@ -19,6 +20,9 @@ const data = [
 ]
 
 const NavOptions = () => {
+
+  const navigation = useNavigation();
+
   return (
     <FlatList
       data={data}
@@ -26,6 +30,7 @@ const NavOptions = () => {
       keyExtractor={(item) => item.id}
       renderItem={ ({item}) => (
         <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
           style={tw` p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 `}
         >
           <View>
