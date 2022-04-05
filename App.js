050d74cb23b6780bba680c138e0e5227b269ef22
Pranav Gaikwad <pranavjigaikwad@gmail.com>
2022-04-05 21:55:53 +0530

    Updated..

--- App.js
+++ App.js
@@ -3,15 +3,22 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
-
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
+
+
   return (
     <Provider store={store} >
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <HomeScreen />
+        </SafeAreaProvider>
+      </NavigationContainer>
+
     </Provider>
   );
 }
