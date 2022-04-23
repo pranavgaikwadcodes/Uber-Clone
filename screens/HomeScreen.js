d484280ed2bcf22fb90a605ee46fd9ad8cf10565
vaishnavi patil <vp397264@gmail.com>
2022-04-23 09:57:55 +0530

    changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
-import React from 'react'
-import tw from 'twrnc'
-import NavOptions from '../components/NavOptions'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import React from 'react';
+import tw from 'twrnc';
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY} from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+
+  const dispatch=useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`} >
@@ -16,6 +23,39 @@ const HomeScreen = () => {
             source={{
                 uri:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png",
             }}
+          />
+
+
+          <GooglePlacesAutocomplete
+           placeholder="Where From?"
+           styles={{
+             container:{
+               flex:0,
+             },
+             textInput:{
+               fontSize:18,
+             },
+
+           }}
+           onPress={(data,details = null) => {
+             dispatch(setOrigin({
+               location:details.geometry.location,
+               description:data.description,
+             }))
+
+             dispatch(setDestination(null));
+           }}
+           fetchDetails={true}
+           enablePoweredByContainer={false}
+           minLength={2}
+           query={{
+             key:GOOGLE_MAPS_APIKEY,
+             language:'en'
+           }}
+           
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          
           />
 
           <NavOptions />
