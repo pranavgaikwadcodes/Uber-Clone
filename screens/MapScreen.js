b4675dfa4e3a47d3204ee4002d6c7f68429332d2
vaishnavi patil <vp397264@gmail.com>
2022-04-23 19:15:36 +0530

    changes

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import tw from 'twrnc';
+//import MapView from 'react-native-maps';
 
 const MapScreen = () => {
   return (
     <View>
-      <Text>MapScreen</Text>
+      <Text>here is the map stuff...</Text>
+
+      <View style={tw`h-1/2`}></View>
+      <map/>
+
+      <view style={tw`h-1/2`}></view>
     </View>
-  )
-}
+  );
+};
 
-export default MapScreen
+export default MapScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
